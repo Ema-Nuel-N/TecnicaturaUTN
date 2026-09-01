ad344a328134be54f46fb73fbf0d2aa7baae4e49
Emanuel Nuñez <emael8744@gmail.com>
2026-08-31 22:14:05 -0300

    Clase 3 Python completa

--- TecnicaturaGit/Python/SegundoSemestre/Clase_3/main.py
+++ TecnicaturaGit/Python/SegundoSemestre/Clase_3/main.py
@@ -56,3 +56,73 @@ print(diccionarioNuevo)
 diccionario2 = {'Ema': {'Edad': 19, 'Altura': 1.78},'Abril':[19, 1.59], 'Martina': [19, 1.62]}
 print(diccionario2['Martina'])
 
+seleccionArgentina = {
+    10:{'Nombre': 'Lionel Messi', 'Edad': 39, 'Altura': 1.70, 'Precio': '50 Millones', 'Posición': 'Extremo Derecho'},
+    19: {'Nombre': 'Nicolás Otamendi', 'Edad': 34, 'Altura': 1.83, 'Precio': '3.5 Millones', 'Posicion': 'Defensa Central'},
+    9:{'Nombre': 'Julían Álvarez', 'Edad': 26, 'Altura': 1.70, 'Precio': '120 Millones', 'Posición': 'Delantero Centro'},
+    24:{'Nombre': 'Enzo Fernandez', 'Edad': 24, 'Altura': 1.78, 'Precio': '100 Millones', 'Posición': 'Mediocampista Central'},
+    13:{'Nombre': 'Cristian Romero', 'Edad': 28, 'Altura': 1.85, 'Precio': '50 Millones', 'Posición': 'Defensa Central'},
+    6:{'Nombre': 'Lisandro Martinez', 'Edad': 28, 'Altura': 1.75, 'Precio': '45 Millones', 'Posición': 'Defensa Central'},
+    23:{'Nombre': 'Emiliano Martínez', 'Edad': 32, 'Altura': 1.95, 'Precio': '35 Millones', 'Posición': 'Arquero'},
+    22:{'Nombre': 'Lautaro Martínez', 'Edad': 27, 'Altura': 1.74, 'Precio': '85 Millones', 'Posición': 'Delantero'},
+    7:{'Nombre': 'Alexis Mac Allister', 'Edad': 26, 'Altura': 1.74, 'Precio': '70 Millones', 'Posición': 'Mediocampista'}
+
+}
+for llave, valor in seleccionArgentina.items():
+    print(llave, valor)
+
+# Como tarea agregar por lo menos 4 jugadores mas al diccionario: seleccionArgentina
+print("Tenemos cargados en el diccionario la cantidad de: ", end="")
+print(len(seleccionArgentina))
+
+# Pilas usando listas
+pila = [1, 2, 3]
+
+# Agregar elementos a la pila por el final
+pila.append(4)
+pila.append(5)
+print(pila)
+
+# Sacamos elementos desde el final
+elementoBorrado = pila.pop() # Quita el último elemento y lo guarda en la variable
+print(f'Sacamos el elemento: {elementoBorrado}')
+print(f'La pila quedo de la siguiente forma: {pila}')
+
+# Colas con listas
+# Estructura de datos de tipo fifo (first input / first output)
+cola = ['Abril','Martina','Emanuel','Graciela','Monica']
+
+# Agregamos elementos al final de la cola
+cola.append('Ulises')
+cola.append('Marcas')
+print(cola)
+
+# Sacamos elementos de la cola
+seRetira = cola.pop(0)
+print(f'Atendido el/la client@: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0)
+print(f'Atendido el/la client@: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0)
+print(f'Atendido el/la client@: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0)
+print(f'Atendido el/la client@: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0)
+print(f'Atendido el/la client@: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0)
+print(f'Atendido el/la client@: {seRetira}')
+print(cola)
+
+seRetira = cola.pop(0)
+print(f'Atendido el/la client@: {seRetira}')
+print(cola)
+
