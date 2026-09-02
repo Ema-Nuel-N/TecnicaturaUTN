8b312bdac072b828f249e7f3b7da225c368349c0
Emanuel Nuñez <emael8744@gmail.com>
2026-09-02 19:46:19 -0300

    Commiteamos leccion 3 clases y objetos en Java

--- TecnicaturaGit/Java/SegundoSemestre/Clase_03/LeccionClasesObjetos/src/Clases/Persona.java
+++ TecnicaturaGit/Java/SegundoSemestre/Clase_03/LeccionClasesObjetos/src/Clases/Persona.java
@@ -4,4 +4,10 @@ public class Persona {
     //Atributos de la clase (Caracteristicas)
     String nombre;
     String apellido;   
+    
+    //Métodos de la clase(Acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
 }
